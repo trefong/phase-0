d0a82a3fdc77d468e54cb8b0fd8a9bd1d093afda
trefong <tre.fong1@gmail.com>
2016-03-03 21:47:02 -0800

    update2

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,16 @@
+# Factorial
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+  # return (1..number).each as first_array entries
+  # tell new array to have the size declared by number
+  # subtract number by index number and multiply it
+  # array entry each do n * (n-i)
+#end
+
+
+def factorial (number)
+  put
+end
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,34 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your middle name?"
+middle_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts 'Hi there ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
+
+
+puts "What is your favorite number?"
+fav_num = gets.chomp
+new_num = fav_num.to_i + 1
+puts new_num.to_s + ' is a much better number'
+
+#How do you define a local variable?
+  #You define a local variable by naming it and setting it equal to something via the equals sign.
+
+#How do you define a method?
+  #You write def and then your method name and any arguments it takes.
+
+#What is the difference between a local variable and a method?
+  #A local variable holds data and gives it a name. A method holds a procedure and gives it a name.
+
+#How do you run a ruby program from the command line?
+  #If the program is in a ruby file, you type ruby and then the name of the file.
+
+#How do you run an RSpec file from the command line?
+  #You would type rspec and then the file name.
+
+#What was confusing about this material? What made sense?
+  #A lot of the times I was confused as to what the directions were asking us to do. There were little details that if they were neglected, the rspec check would return failures.What made sense was the concept that of how methods use local variables.
