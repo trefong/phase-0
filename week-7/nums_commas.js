7ca1934ac8df5c6cd770a55f2fc80ce167060911
trefong <tre.fong1@gmail.com>
2016-03-23 14:47:49 -0700

    Do challenge reflection questions for 7.5

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -23,7 +23,7 @@ var separateComma = function(input_integer) {
   var i = input_integer.toString();
   var characters = i.split("");
   var rev_characters = characters.reverse();
-  // var add_comma = [];
+  var add_comma = [];
   for (var counter = 0; counter < rev_characters.length; counter++){
     if ((counter+1) % 4 == 0)
      var add_comma = rev_characters.splice(counter, 0, ',');
@@ -70,3 +70,16 @@ console.log(separateComma(2234998759675));
 
   // separateComma(2234);
 
+/*
+What was it like to approach the problem from the perspective of Javascript? Did you approach the problem differently?
+Our approach was actually the same to the approach we both used with Ruby. After discussing how we wanted to solve this challenge, we felt that that was still the best way.
+
+What did you learn about iterating over arrays in JavaScript?
+I learned alot about JavaScript syntax in this challenge. We had some difficulty knowing where to put brackets and semicolons.
+
+What was different about solving this problem in JavaScript?
+Knowing how the program will read the code was what was different this time around, also, knowing the different syntax.
+
+What built-in methods did you find to incorporate in your refactored solution?
+We used a lot of built-in methods including, reverse, split, to string, splice, and join.
+/*
